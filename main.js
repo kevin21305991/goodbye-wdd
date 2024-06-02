15c120072e63fbfb787b9a4884c477c0b4cb6092
Kevin <kevin.21305991@gmail.com>
2024-06-02 10:19:28 +0800

    20240602

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, set, update, push, child, onValue, get } from 'firebase/database';
 import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
 import { gsap } from 'gsap';
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Pagination, EffectFade } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
 
 const accountInput = document.querySelector('input[name="account"]');
 const passwordInput = document.querySelector('input[name="password"]');
@@ -11,7 +18,8 @@ const errorText = document.querySelector('.error-text');
 const envelope = document.querySelector('.envelope');
 const envelopeFlap = document.querySelector('.envelope .front.flap');
 const letter = document.querySelector('.letter');
-const letterInner = document.querySelector('.letter .inner');
+const letterInner = document.querySelector('.letter > .inner');
+const swiperWrapper = document.querySelector('.letter > .inner .swiper-wrapper');
 
 /**
  * firebase 連線
@@ -42,20 +50,36 @@ async function firebaseLogin(email, password) {
 }
 
 /**
+ * HTML 字串轉 DOM
+ * @param {string} htmlString HTML 字串
+ * @returns
+ */
+function htmlStringToDOM(htmlString) {
+  const dom = document.createElement('div');
+  dom.innerHTML = htmlString;
+  return dom.children[0];
+}
+
+/**
  * 產生內容
  * @param {object} data 資料物件
  */
 function generateContent(data) {
-  const dom = `
-    <div class="start">Dear <span class="name">${data.name}</span></div>
-    <div class="content">
-      <div class="scroll-area">
-        <div class="inner">${data.content}</div>
+  data.forEach(item => {
+    const domString = `
+      <div class="swiper-slide">
+        <div class="start">Dear <span class="name">${item.name}</span></div>
+        <div class="content">
+          <div class="scroll-area">
+            <div class="inner">${item.content}</div>
+          </div>
+        </div>
+        <div class="end">By <span class="name">Kevin</span></div>
       </div>
-    </div>
-    <div class="end">By <span class="name">Kevin</span></div>
-  `;
-  letterInner.innerHTML = dom;
+    `;
+    const dom = htmlStringToDOM(domString);
+    swiperWrapper.appendChild(dom);
+  });
 }
 
 /**
@@ -67,11 +91,61 @@ function loadData(email) {
   const dbRef = ref(db, '/users/');
   onValue(dbRef, snapshot => {
     if (!snapshot.exists()) return;
-    const AllData = Object.values(snapshot.val()).map(item => item);
-    const data = AllData.find(item => item.account === email);
-    generateContent(data);
+    const AllData = Object.values(snapshot.val())
+      .map(item => item)
+      .sort();
+    const sortData = AllData.sort((a, b) => {
+      const accountA = a.account.toLowerCase();
+      const accountB = b.account.toLowerCase();
+
+      if (accountA < accountB) return -1;
+      if (accountA > accountB) return 1;
+      return 0;
+    });
+    console.log(sortData);
+    const data = AllData.filter(item => item.account === email);
+    if (data.length === 0) {
+      generateContent(AllData);
+    } else {
+      generateContent(data);
+    }
+    // init Swiper
+    const swiper = new Swiper('.swiper', {
+      // configure Swiper to use modules
+      modules: [Pagination, EffectFade],
+      on: {
+        slideChangeTransitionStart() {
+          const viewH = window.innerHeight;
+          const activeSlide = this.el.querySelector('.swiper-slide-active');
+          const envelopeFlapH = envelopeFlap.offsetHeight;
+          const envelopeH = getEnvelopeSize().h + envelopeFlapH;
+          const slideHeight = activeSlide.clientHeight >= 0.84 * viewH ? 0.84 * viewH : activeSlide.clientHeight;
+          gsap.to('.envelope .letter', {
+            duration: 0.8,
+            ease: 'power1.out',
+            top: (viewH - slideHeight) / 2,
+            transform: `translate(-50%, ${-(viewH - envelopeH) / 2 - envelopeFlapH}px)`,
+            height: slideHeight,
+            onComplete: () => {
+              showLetterAnimate().play();
+            },
+          });
+        },
+      },
+      speed: 800,
+      autoHeight: true,
+      centeredSlides: true,
+      effect: 'fade',
+      fadeEffect: {
+        crossFade: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+      },
+    });
     const allAnimate = gsap.timeline();
-    allAnimate.add(loginAnimate()).add(envelopeAnimate());
+    allAnimate.add(loginAnimate()).add(envelopeAnimate()).add(showLetterAnimate());
   });
 }
 
@@ -113,7 +187,7 @@ function observeEnvelopeSize() {
 
 /**
  * 登入動畫
- * @returns
+ * @returns gsap timeline
  */
 function loginAnimate() {
   const tl = gsap.timeline();
@@ -137,7 +211,7 @@ function loginAnimate() {
 
 /**
  * 信封打開動畫
- * @returns
+ * @returns gsap timeline
  */
 function envelopeAnimate() {
   const tl = gsap.timeline();
@@ -177,13 +251,22 @@ function envelopeAnimate() {
     top: (viewH - letterInnerH) / 2,
     transform: `translate(-50%, ${-(viewH - envelopeH) / 2 - envelopeFlapH}px)`,
   });
-  tl.to('.envelope .letter .content p', {
+  return tl;
+}
+
+/**
+ * 顯示信件內容動畫
+ * @returns gsap timeline
+ */
+function showLetterAnimate() {
+  const tl = gsap.timeline();
+  tl.to('.letter .swiper-slide-active .content p', {
     duration: 0.5,
     ease: 'power1.out',
     '--w': 0,
     stagger: 0.3,
   });
-  tl.to('.envelope .letter .end', {
+  tl.to('.letter .swiper-slide-active .end', {
     duration: 0.3,
     ease: 'power1.out',
     clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
@@ -273,4 +356,6 @@ function loginHandler() {
       loginBtn.classList.remove('active');
     }
   });
+
+  window.envelopeAnimate = envelopeAnimate;
 })();
